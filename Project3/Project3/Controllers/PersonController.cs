8fc4987bb03d947dfd3960b70d6654eb33755731
ZeynepHande <zeynephandega@gmail.com>
2022-11-08 00:43:35 +0300

    Project3

---
+++ Project3/Project3/Controllers/PersonController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Project3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project3.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Iletisim()
+        {
+            return View();
+        }
+        public IActionResult Info()
+        {
+            var jsonPersons = JsonConvert.SerializeObject(persons);
+            return Json(jsonPersons);
+        }
+        public static List<Person> persons = new List<Person>
+        {
+            new Person
+            {
+                Ad = "Hande",
+                Soyad = "Altuner",
+                Tarih = DateTime.Today,
+                Yas = 32,
+                YasadigiSehir = "İstanbul"
+            }
+        };
+    }
+}
---
+++ Project3/Project3/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project3.Models
+{
+    public class Person
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public DateTime Tarih { get; set; }
+        public int Yas { get; set; }
+        public string YasadigiSehir { get; set; }
+    }
+}
